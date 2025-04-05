f9e511db702d35b055c03204d52bbfc291345244
ElHuzain <a.huzain98@gmail.com>
2025-04-05 11:08:18 +0200

    Add typography styles to global CSS and update button component for improved design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 'use client';
 import "./globals.css";
 import useThemeInitialize from "@/hooks/useThemeInitialize";
+import { Plus_Jakarta_Sans } from 'next/font/google'
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+  variable: '--font-plus-jakarta-sans',
+})
 
 export default function RootLayout({
   children,
@@ -8,11 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   useThemeInitialize()
-
   return (
     <html lang="en">
       <body
-        className={`antialiased text-text-secondary`}
+        className={`antialiased text-text-secondary ${plusJakartaSans.className}`}
       >
         {children}
       </body>
