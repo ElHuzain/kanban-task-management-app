097699710501d219af87f5956a6e6b949523652a
ElHuzain <a.huzain98@gmail.com>
2025-04-05 11:32:11 +0200

    Add UI for subtask checkbox and basic functionality

---
+++ components/ui/subtaskCheckbox.tsx
@@ -0,0 +1,18 @@
+import { Label } from "./label";
+import { Checkbox } from "./checkbox";
+
+const SubtaskCheckbox = ({ subtask }: { subtask: SubTask }) => {
+    const { isCompleted, title } = subtask;
+
+    const CheckedStyle = isCompleted ? "line-through text-text-title/50" : "";
+    return (
+        <Label className="flex items-center gap-4 bg-bg-deep w-fit p-4 rounded-sm group hover:bg-primary/20">
+            <Checkbox checked={isCompleted} className="bg-bg h-4 w-4 border-2 border-lines" />
+            <span className={`text-text-title body-bold ${CheckedStyle}`}>
+                {title}
+            </span>
+        </Label>
+    );
+};
+
+export default SubtaskCheckbox;
