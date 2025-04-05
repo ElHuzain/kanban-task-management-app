4f12da8fe34d7097898c4609f503ef14667f336e
ElHuzain <a.huzain98@gmail.com>
2025-04-05 10:21:17 +0200

    Update global styles and button component for improved theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={`antialiased text-text-secondary`}
       >
         {children}
       </body>
