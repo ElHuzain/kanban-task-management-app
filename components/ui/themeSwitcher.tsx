5b2dbcf5df6e09a3159bd58551747d31512081b4
ElHuzain <a.huzain98@gmail.com>
2025-04-05 21:58:22 +0200

    Refactored ThemeSwitcher to adapt with design and improved functionality

--- components/ui/themeSwitcher.tsx
+++ components/ui/themeSwitcher.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { getCurrentTheme, switchTheme } from "@/lib/utils"
 import { ChangeEvent, useEffect, useState } from "react"
-
+import { Checkbox } from "./checkbox"
+import { Label } from "./label"
+import { cn } from "@/lib/utils"
+import Image from "next/image"
 const ThemeSwitcher = () => {
     const [theme, setTheme] = useState<Theme>('system')
     
@@ -10,20 +13,23 @@ const ThemeSwitcher = () => {
         setTheme(currentTheme)
     }, [])
 
-    const handleThemeChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        const theme = e.target.value as Theme
-
-        setTheme(theme)
-        switchTheme(theme)
+    const handleThemeChange = (checked: boolean) => {
+        const newTheme = checked ? 'dark' : 'light' as Theme
+        setTheme(newTheme)
+        switchTheme(newTheme)
     }
 
+    const isChecked = theme === 'dark'
+
     return (
-        <div>
-            <select value={theme} onChange={handleThemeChange}>
-                <option value="dark">Dark</option>
-                <option value="light">Light</option>
-                <option value="system">System</option>
-            </select>
+        <div className="bg-card flex items-center gap-4 p-4 rounded-sm w-fit">
+            <Image src="/icon-light-theme.svg" alt="Light Theme" width={15} height={15} />
+            <Label className="flex items-center gap-4 w-10 h-5 p-1 rounded-full bg-primary relative">
+                <div className={cn("w-3 h-3 rounded-full absolute left-1 bg-white transition-transform duration-300", isChecked && "translate-x-[20px]")}></div>
+                <span className="sr-only">Change theme</span>
+                <Checkbox className="sr-only" checked={isChecked} onCheckedChange={handleThemeChange} /> 
+            </Label>
+            <Image src="/icon-dark-theme.svg" alt="Dark Theme" width={15} height={15} />
         </div>
     )
 }
