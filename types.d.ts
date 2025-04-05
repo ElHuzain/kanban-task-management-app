2392b99bf6421172f8ea464221bcab22712dbe78
ElHuzain <a.huzain98@gmail.com>
2025-04-05 09:52:21 +0200

    Create task types based on provided JSON temp data

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,25 @@
 type Theme = 'dark' | 'light' | 'system'
+
+type SubTask = {
+    title: string
+    isCompleted: boolean
+}
+
+type Task = {
+    title: string
+    description: string
+    status: string
+    subtasks: SubTask[]
+}
+
+type Column = {
+    id: string
+    name: string
+    tasks: Task[]
+}
+
+type Board = {
+    id: string
+    name: string
+    columns: Column[]
+}
