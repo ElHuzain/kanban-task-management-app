c7312f2b1c4f1347d3242104906ffe5ab744b4c0
ElHuzain <a.huzain98@gmail.com>
2025-04-05 11:57:28 +0200

    Add Input and LabelledInput components with styling

---
+++ components/ui/labelledInput.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { cn } from "@/lib/utils";
+import { Input } from "./input";
+import { Label } from "./label";
+
+interface LabelledInputProps {
+    label: string;
+    value: string;
+    error?: string;
+    placeholder: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const LabelledInput = (props: LabelledInputProps) => {
+    const {
+        label,
+        value,
+        error,
+        placeholder,
+        onChange
+    } = props;
+
+    return (
+        <Label className="flex flex-col gap-2 text-start items-start">
+            <span>{label}</span>
+            <Input className={cn("rounded-sm border-text-secondary/25 border", error && "border-destructive")} value={value} placeholder={placeholder} onChange={onChange} error={error} />
+        </Label>
+    )
+}
+
+export default LabelledInput;
